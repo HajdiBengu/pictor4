ff8448f051ee3b422525e07f4925214ae8765de3
HajdiBengu <44471633+HajdiBengu@users.noreply.github.com>
2023-02-23 10:55:41 -0800

    get bid amount parameter

--- src/auction.tsx
+++ src/auction.tsx
@@ -1,8 +1,17 @@
 // resume on min 30
 
+import { useState } from "react";
 import { bidOperation } from "./utils/operation";
 
 const Auction = () => {
+
+    const [tez, setTez] = useState("");
+
+    const returnInput = async () => {
+        let input = document.getElementById("bid");
+        setTez(input);
+        alert(tez)
+    };
 
     const onBid = async () => {
         await bidOperation();
@@ -11,9 +20,9 @@ const Auction = () => {
     return ( 
     <div className="grid">
     <div className="image">image</div>
-    <input type="text" className="bid" placeholder="tez" />
+    <input type="text" className="bid" id="bid" placeholder="tez" />
     <input onClick={onBid} type="button" className="placeBid" value="Place Bid" />
-    <input type="button" className="cancelBid" value="Cancel Bid" />
+    <input onClick={returnInput} type="button" className="cancelBid" value="Cancel Bid" />
     <p className="topBid">Currect Bid: <span className="bidspan">CurrentBid Tez</span></p>
     <div className="timer">
     </div>
--- src/utils/operation.tsx
+++ src/utils/operation.tsx
@@ -1,10 +1,10 @@
 import { tezos } from "./tezos";
 
-export const bidOperation = async () => {
+export const bidOperation = async (tez: number) => {
     try{
         const contract = await tezos.wallet.at('KT1SXvgcy4jUQpJK8Q2dA8cNZs8bLxTj1E2r');
         const op = await contract.methods.bid().send({
-            amount: 1,
+            amount: tez,
             mutez: false,
         });
         await op.confirmation(1); 
