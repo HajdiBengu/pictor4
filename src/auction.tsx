9981b51028b4ba8595b5337b80b41543be89268f
HajdiBengu <44471633+HajdiBengu@users.noreply.github.com>
2023-02-23 15:10:02 -0800

    bid operation live

--- src/auction.tsx
+++ src/auction.tsx
@@ -5,24 +5,23 @@ import { bidOperation } from "./utils/operation";
 
 const Auction = () => {
 
-    const [tez, setTez] = useState("");
-
-    const returnInput = async () => {
-        let input = document.getElementById("bid");
-        setTez(input);
-        alert(tez)
-    };
-
+    const [tez, setTez] = useState("")
+    const setBid = (event: React.ChangeEvent<HTMLInputElement>) => {
+        let bidAmount: string = event.target.value;
+        setTez(bidAmount)
+    }
+   
     const onBid = async () => {
-        await bidOperation();
+        // let y: number = +{tez};
+        await bidOperation(Number(tez));
     };
 
     return ( 
     <div className="grid">
     <div className="image">image</div>
-    <input type="text" className="bid" id="bid" placeholder="tez" />
+    <input onChange={setBid} type="text" className="bid" id="bid" placeholder="tez" value={tez}/>
     <input onClick={onBid} type="button" className="placeBid" value="Place Bid" />
-    <input onClick={returnInput} type="button" className="cancelBid" value="Cancel Bid" />
+    <input type="button" className="cancelBid" value="Cancel Bid" />
     <p className="topBid">Currect Bid: <span className="bidspan">CurrentBid Tez</span></p>
     <div className="timer">
     </div>
