6a08f068d7380f8862fc196afbccd7d25267a58d
HajdiBengu <44471633+HajdiBengu@users.noreply.github.com>
2023-02-25 16:31:25 -0800

    added axios

---
+++ src/utils/tzkt.tsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+//TODO check if this function needs await
+export const fetch = async () => {
+    try {
+        const res = await axios.get("https://api.ghostnet.tzkt.io/v1/contracts/KT1SXvgcy4jUQpJK8Q2dA8cNZs8bLxTj1E2r/storage");
+        return res.data;
+    } catch (error) {
+        throw error;
+    }
+};
