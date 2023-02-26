01a54b01676c0ee22568e80db259db9f96a774f5
HajdiBengu <44471633+HajdiBengu@users.noreply.github.com>
2023-02-26 14:58:45 -0800

    add await

--- src/utils/tzkt.tsx
+++ src/utils/tzkt.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 //TODO check if this function needs await
 export const fetch = async () => {
     try {
-        const res = axios.get("https://api.ghostnet.tzkt.io/v1/contracts/KT1SXvgcy4jUQpJK8Q2dA8cNZs8bLxTj1E2r/storage");
+        const res = await axios.get("https://api.ghostnet.tzkt.io/v1/contracts/KT1SXvgcy4jUQpJK8Q2dA8cNZs8bLxTj1E2r/storage");
         return res.data;
     } catch (error) {
         throw error;
