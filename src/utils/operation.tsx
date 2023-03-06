60d76bd8a721033512bd93a037fd3f03875f970c
HajdiBengu <44471633+HajdiBengu@users.noreply.github.com>
2023-03-06 15:03:43 -0800

    updated auction contract

--- src/utils/operation.tsx
+++ src/utils/operation.tsx
@@ -15,7 +15,7 @@ export const bidOperation = async (tez: number) => {
 
 export const cancelBidOperation = async () => {
     try{
-        const contract = await tezos.wallet.at('KT1SXvgcy4jUQpJK8Q2dA8cNZs8bLxTj1E2r');
+        const contract = await tezos.wallet.at('KT1DgbWd3pakHMzUpc83ANSMdAQHr8VAVhB9');
         const op = await contract.methods.cancel_bid().send();
         await op.confirmation(1); 
     }catch(err){
@@ -25,7 +25,7 @@ export const cancelBidOperation = async () => {
 
 export const endAuctionOperation = async () => {
     try{
-        const contract = await tezos.wallet.at('KT1SXvgcy4jUQpJK8Q2dA8cNZs8bLxTj1E2r');
+        const contract = await tezos.wallet.at('KT1DgbWd3pakHMzUpc83ANSMdAQHr8VAVhB9');
         const op = await contract.methods.end_auction().send();
         await op.confirmation(1); 
     }catch(err){
@@ -35,7 +35,7 @@ export const endAuctionOperation = async () => {
 
 export const startAuctionOperation = async () => {
     try{
-        const contract = await tezos.wallet.at('KT1SXvgcy4jUQpJK8Q2dA8cNZs8bLxTj1E2r');
+        const contract = await tezos.wallet.at('KT1DgbWd3pakHMzUpc83ANSMdAQHr8VAVhB9');
         const op = await contract.methods.restart_auction().send();
         await op.confirmation(1); 
     }catch(err){
--- src/utils/tzkt.tsx
+++ src/utils/tzkt.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 //TODO check if this function needs await
 export const fetchStorage = async () => {
     try {
-        const res = await axios.get("https://api.ghostnet.tzkt.io/v1/contracts/KT1SXvgcy4jUQpJK8Q2dA8cNZs8bLxTj1E2r/storage");
+        const res = await axios.get("https://api.ghostnet.tzkt.io/v1/contracts/KT1DgbWd3pakHMzUpc83ANSMdAQHr8VAVhB9/storage");
         return res.data;
     } catch (error) {
         throw error;
